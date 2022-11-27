c2f14e40e0137f63301d859b30675b23e7a45a9a
Ethan Reece <git@ethanreece.com>
2022-11-27 14:02:59 -0600

    Add more things to tile generator

--- Assets/Scripts/GridSpawner.cs
+++ Assets/Scripts/GridSpawner.cs
@@ -6,28 +6,42 @@ using UnityEngine;
 public class GridSpawner : MonoBehaviour
 {
 
-    public GameObject wall;
+    public GameObject cobblestoneWall;
+    public GameObject cobblestone;
     public GameObject player;
     public GameObject slime;
     public GameObject icebubble;
+    public GameObject desertbubble;
+    public GameObject firebubble;
+    public GameObject desertgun;
+    public GameObject forestgun;
+    public GameObject tundragun;
+    public GameObject volcanogun;
+    public GameObject boss;
+    public GameObject grass;
+    public GameObject mud;
+    public GameObject sand;
+    public GameObject ice;
+    public GameObject snow;
+
     private GameObject tile;
     // Start is called before the first frame update
     void Start()
     {
         string map =
             @"
-000000000000000
-00000000S000000
-000000000001000
-000P00000001000
-0000000000010I0
-000000001111110
-000000000001000
-00000000S0010I0
-000000000001000
-000000000000000
-000000000000000
-000000000000000
+_______________
+________S______
+___________|___
+^^____*____|___
+^^_________|_I_
+vv______||||||_
+.._________|___
+//______S__|_D_
+--_________|___
+_d__l________F_
+_______________
+_f__t_____!____
 ";
         var gameArray = map.Split(Environment.NewLine);
         var game2DArray = new char[gameArray.Length - 2, gameArray[1].Length];
@@ -41,22 +55,64 @@ public class GridSpawner : MonoBehaviour
         }
         
         for (int i = 0; i < game2DArray.GetLength(0); i++) {
-            for (int j = 0; j < game2DArray.GetLength(1); j++) {
-                if (game2DArray[i, j] == '1') {
-                    tile = wall;
-                } 
-                if (game2DArray[i, j] == 'P') {
-                    tile = player;
-                } 
-                if (game2DArray[i, j] == 'S') {
-                    tile = slime;
-                } 
-                if (game2DArray[i, j] == 'I') {
-                    tile = icebubble;
-                } 
-                if (game2DArray[i, j] != '0') {
-                    Instantiate(tile, new Vector3(j+0.5f, 11.5f-i, 0f), Quaternion.identity);
+            for (int j = 0; j < game2DArray.GetLength(1); j++)
+            {
+                switch(game2DArray[i, j])
+                {
+                    case '|':
+                        tile = cobblestoneWall;
+                        break;
+                    case '*':
+                        tile = player;
+                        break;
+                    case 'S':
+                        tile = slime;
+                        break;
+                    case 'I':
+                        tile = icebubble;
+                        break;
+                    case 'D':
+                        tile = desertbubble;
+                        break;
+                    case 'F':
+                        tile = firebubble;
+                        break;
+                    case 'd':
+                        tile = desertgun;
+                        break;
+                    case 'f':
+                        tile = forestgun;
+                        break;
+                    case 't':
+                        tile = tundragun;
+                        break;
+                    case 'l':
+                        tile = volcanogun;
+                        break;
+                    case '!':
+                        tile = boss;
+                        break;
+                    case '^':
+                        tile = grass;
+                        break;
+                    case 'v':
+                        tile = mud;
+                        break;
+                    case '.':
+                        tile = sand;
+                        break;
+                    case '/':
+                        tile = ice;
+                        break;
+                    case '-':
+                        tile = snow;
+                        break;
+                    case '_':
+                    default:
+                        tile = cobblestone;
+                        break;
                 }
+                Instantiate(tile, new Vector3(j+0.5f, 11.5f-i, 0f), Quaternion.identity);
             }
         }
     }
