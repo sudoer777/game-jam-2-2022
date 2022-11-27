4238e8a04ad76f1e2acae08082f768f517d0f10d
Josiah Kiok <josiahkiok@Josiahs-MacBook-Pro.local>
2022-11-27 16:08:37 -0600

    IceBubble firing script, fix player shot bug

--- Assets/Scripts/EnemyMovement/MovSShooter.cs
+++ Assets/Scripts/EnemyMovement/MovSShooter.cs
@@ -2,30 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovVShooter : Enemy
+public class MovSShooter : Enemy
 {
     private float lifespan = 0;
     Vector3 playerPos;
     Vector3 direction;
     public Projectile EnemyProjectile;
+    
     public void UpdateLifespan() {
         lifespan += Time.deltaTime;
     }
 
     public override void Update()
     {
-        if ((lifespan % 1.2) < 0.5 && ((lifespan + Time.deltaTime) % 1.2) > 0.5) {
-            for (float i = 0f; i < 2.1; i++) {
-                float enemyProjectileSpeed = 9f+2*i;
+        if ((lifespan % 2.5) < 1.2 && ((lifespan + Time.deltaTime) % 2.5) > 1.2) {
+            for (int i = 0; i < 10; i++) {
+                float enemyProjectileSpeed = 8f+Random.Range(0f, 5f);
                 playerPos = Player.Instance.transform.position;
                 float ang = Mathf.Atan2((playerPos.y-transform.position.y),(playerPos.x-transform.position.x));
-                direction = new Vector3(Mathf.Cos(ang+0.3f), Mathf.Sin(ang+0.3f), 0);
+                float deviation = Random.Range(-1.5f, 1.5f);
+                direction = new Vector3(Mathf.Cos(ang+deviation), Mathf.Sin(ang+deviation), 0);
                 var projectile = Instantiate(EnemyProjectile, transform.position+direction*0.4f, transform.rotation);
                 projectile.Fire(enemyProjectileSpeed, direction);
-                direction = new Vector3(Mathf.Cos(ang-0.3f), Mathf.Sin(ang-0.3f), 0);
-                var projectile2 = Instantiate(EnemyProjectile, transform.position+direction*0.4f, transform.rotation);
-                projectile2.Fire(enemyProjectileSpeed, direction);
-            }
+            }   
         }
         UpdateLifespan();
     }
