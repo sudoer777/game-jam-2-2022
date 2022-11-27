80648f6fd2752cf6e30b18224ca2906f75160e48
Ethan Reece <git@ethanreece.com>
2022-11-27 13:05:45 -0600

    Delete stuff

--- Assets/Scripts/GridSpawner.cs
+++ Assets/Scripts/GridSpawner.cs
@@ -14,9 +14,6 @@ public class GridSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        /*string [] gameArray = new string[] {
-            "P"
-        };*/
         string map =
             @"
 000000000000000
@@ -43,29 +40,6 @@ public class GridSpawner : MonoBehaviour
             }
         }
         
-        /*string[] gameArray = new string[] {
-            "000000000000000",
-            "00000000S000000",
-            "000000000001000",
-            "000P00000001000",
-            "0000000000010I0",
-            "000000001111110",
-            "000000000001000",
-            "00000000S0010I0",
-            "000000000001000",
-            "000000000000000",
-            "000000000000000",
-            "000000000000000",
-        };*/
-        /*
-        char[,] game2DArray = new char[gameArray.Length - 2,gameArray[1].Length];
-        for (int i = 1; i < gameArray.Length - 1; i++) {
-            char[] text = gameArray[i].ToCharArray();
-            for (int j = 0; j < gameArray[i].Length; j++) {
-                game2DArray[i,j] = text[j];
-            }
-        }
-        */
         for (int i = 0; i < game2DArray.GetLength(0); i++) {
             for (int j = 0; j < game2DArray.GetLength(1); j++) {
                 if (game2DArray[i, j] == '1') {
@@ -81,7 +55,7 @@ public class GridSpawner : MonoBehaviour
                     tile = icebubble;
                 } 
                 if (game2DArray[i, j] != '0') {
-                Instantiate(tile, new Vector3(j+0.5f, 11.5f-i, 0f), Quaternion.identity);
+                    Instantiate(tile, new Vector3(j+0.5f, 11.5f-i, 0f), Quaternion.identity);
                 }
             }
         }
