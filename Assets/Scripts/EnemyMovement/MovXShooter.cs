f7b882cf3c2257b76937561bd9b3b9fdb1449eab
Ethan Reece <git@ethanreece.com>
2022-11-27 19:15:51 -0600

    Increase enemy projectile speed

--- Assets/Scripts/EnemyMovement/MovXShooter.cs
+++ Assets/Scripts/EnemyMovement/MovXShooter.cs
@@ -15,7 +15,7 @@ public class MovXShooter : Enemy
     public override void Update()
     {
         if ((lifespan % 1.5) < 0.5 && ((lifespan + Time.deltaTime) % 1.5) > 0.5) {
-            float enemyProjectileSpeed = 6f;
+            float enemyProjectileSpeed = 12f;
             for (int i = 0; i < 4; i++) {
                 Vector3 direction = new Vector3(Mathf.Cos(Mathf.PI/2*i+Mathf.PI/4), Mathf.Sin(Mathf.PI/2*i+Mathf.PI/4), 0);
                 var projectile = Instantiate(EnemyProjectile, transform.position+direction*0.4f, transform.rotation);
